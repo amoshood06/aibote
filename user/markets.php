78e6e594cb5245fb29bac1b1c27caa7a3d380d43
amoshood06 <amoshood06@gmail.com>
2025-02-14 14:53:13 +0100

    nwiouduid

--- user/markets.php
+++ user/markets.php
@@ -106,7 +106,7 @@ $btc_value = ($btc_rate > 0) ? ($user_balance / $btc_rate) : 0;
                         <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+  <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
   {
   "isTransparent": false,
@@ -119,7 +119,7 @@ $btc_value = ($btc_rate > 0) ? ($user_balance / $btc_rate) : 0;
   "locale": "en"
 }
   </script>
-</div>
+
 <!-- TradingView Widget END -->
                        
     </div>
